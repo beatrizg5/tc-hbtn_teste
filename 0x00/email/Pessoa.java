a9c0cd8bdebb99b759d39df9e7506f65eaa93e0e
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-18 15:43:14 -0300

    teste

--- 0x00/email/Pessoa.java
+++ 0x00/email/Pessoa.java
@@ -3,7 +3,7 @@ public class Pessoa {
     public static boolean emailValid(String email){
         if(email.contains("@") && email.length() <= 50){
             return true;
-        }else{
+        }else {
             return false;
         }
     }
--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -4,10 +4,16 @@ public class TesteEmail {
     }
 
     public static boolean testar_email_sem_arroba() {
-        return Pessoa.emailValid("email_testedominio.com.br");
+        if(Pessoa.emailValid("email_testedominio.com.br")){
+            testar_email_com_arroba();
+        }
+        return true;
     }
 
     public static boolean testar_email_mais_50_caracteres() {
-        return Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
+        if(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br")) {
+            testar_email_mais_50_caracteres();
+        }
+        return true;
     }
 }
