6d411090b9124d6f101a140d5fb57bb4593b1713
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-18 16:34:14 -0300

    teste

--- 0x00/calculadora/OperacaoBasica.java
+++ 0x00/calculadora/OperacaoBasica.java
@@ -1,7 +1,14 @@
 public class OperacaoBasica {
-    public void somar(){}
-    public void subtrair(){}
-    public void multiplicar(){}
-    public void dividir(){}
-
+    public static double somar(double a, double b){
+        return a + b;
+    }
+    public static double subtrair(double a, double b){
+        return a - b;
+    }
+    public static double multiplicar(double a, double b){
+        return a * b;
+    }
+    public static double dividir(double a, double b){
+        return a / b;
+    }
 }
