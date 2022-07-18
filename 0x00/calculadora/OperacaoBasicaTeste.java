cb3f914a924dfd0039014bfd3805ef070490c90c
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-18 16:00:48 -0300

    teste

---
+++ 0x00/calculadora/OperacaoBasicaTeste.java
@@ -0,0 +1,17 @@
+public class OperacaoBasicaTeste {
+    public static boolean testar_soma() {
+        return true;
+    }
+
+    public static boolean testar_subtracao() {
+        return true;
+    }
+
+    public static boolean testar_multiplicacao() {
+        return true;
+    }
+
+    public static boolean testar_divisao() {
+        return true;
+    }
+}
