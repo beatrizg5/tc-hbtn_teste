1d19874b01ebabf6b614534bcfa02ba568a01894
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-21 14:32:13 -0300

    teste

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.params.ParameterizedTest;
 
 public class PersonTest {
 
     Person person;
-    @BeforeEach
+    @BeforeAll
     public void setUp() {
         person = new Person();
     }
