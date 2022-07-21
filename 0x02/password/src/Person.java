c2a36b9df494586293edfbf109cd3bddaa52bf4a
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-07-21 16:31:16 -0300

    teste

--- 0x02/password/src/Person.java
+++ 0x02/password/src/Person.java
@@ -1,11 +1,45 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Person {
-    public String checkUser(String user, String neilArms2){
-        if(!user.matches("(?=.[}{,.^?~=+\\-_\\/\\-+.\\|])(?=.*[0-9]).{8,}"));
-        return user;
+    private String user;
+    private String password;
+
+    public boolean checkUser() {
+        int minLength = 8;
+        Pattern pattern = Pattern.compile(".\\W");
+        Matcher matcher = pattern.matcher(user);
+
+        boolean lengthValid = user.length() >= minLength;
+        boolean containsSpecialCharacters = matcher.find();
+
+        return lengthValid && !containsSpecialCharacters;
     }
 
-    public String checkPassword(String password){
-         if(password.matches("(?=.[}{,.^?~=+\\-_\\/\\-+.\\|])(?=.[a-zA-Z])(?=.[0-9]).{8,}"));
-        return password;
+    public boolean checkPassword() {
+        int minLength = 8;
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(password);
+
+        boolean containsNumers = matcher.find();
+        boolean lengthValid = password.length() >= minLength;
+
+        pattern = Pattern.compile("\\d", Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(password);
+        boolean containsUpperCaseChar = matcher.find();
+
+        pattern = Pattern.compile(".\\W");
+        matcher = pattern.matcher(password);
+        boolean containsSpecialCharacters = matcher.find();
+
+        return containsNumers && lengthValid && containsUpperCaseChar && containsSpecialCharacters;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,43 +1,58 @@
 import org.junit.jupiter.api.BeforeAll;
-
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersonTest {
+    static Person person;
 
-    Person person;
     @BeforeAll
-    public void setUp() {
+    public static void setup() {
         person = new Person();
     }
 
-    @ParameterizedTest
-    public void check_user_valid(){
-        person.checkUser("PaulMcCartney2", "NeilArms2");
+    @ParameterizedTest(name = "checkUser, deve ser válido valor: {0}")
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    public void check_user_valid(String user) {
+        person.setUser(user);
     }
 
-    @ParameterizedTest
-    public void check_user_not_valid(){
-        person.checkUser("Paul#McCartney", "Neil@Arms");
-
-    }
-    @ParameterizedTest
-    public void does_not_have_letters(){
-        person.checkPassword("123456789", "#$%1234");
-
-    }
-    @ParameterizedTest
-    public void does_not_have_numbers(){
-        person.checkPassword(“Abcabcdefgh@”, “#hbtn@%tc”);
-
-    }
-    @ParameterizedTest
-    public void does_not_have_eight_chars(){
-        person.checkPassword(“Abc@123”, “12$@hbt”);
-    }
-    @ParameterizedTest
-    public void check_password_valid(){
-        person.checkPassword(“abC123456$”, “Hbtn@1234”, “Betty@1#2”, “Hbtn@123”);
-
+    @ParameterizedTest(name = "checkUser, não deve ser válido valor: {0}")
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+    public void check_user_not_valid(String user) {
+        person.setUser(user);
+        assertFalse(person.checkUser());
     }
 
+    @ParameterizedTest(name = "checkPassword - sem letras, não deve ser válido valor: {0}")
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    public void does_not_have_letters(String password) {
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
+    }
+
+    @ParameterizedTest(name = "checkPassword - sem numeros, não deve ser válido valor: {0}")
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    public void does_not_have_numbers(String password) {
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
+    }
+
+    @ParameterizedTest(name = "checkPassword - menos que 8, não deve ser válido valor: {0}")
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
+    public void does_not_have_eight_chars(String password) {
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
+    }
+
+    @ParameterizedTest(name = "checkPassword, deve ser válido valor: {0}")
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    public void check_password_valid(String password) {
+        person.setPassword(password);
+        assertTrue(person.checkPassword());
+    }
 }
